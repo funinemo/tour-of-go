0d0378c8cc6c20fb217af27007e4feca91b6cf19
funinemo@gmail.com <funinemo@gmail.com>
2020-05-16 21:40:36 +0900

    Exercise: Slices

--- moretypes-18.go
+++ moretypes-18.go
@@ -2,14 +2,14 @@ package main
 
 import "golang.org/x/tour/pic"
 
-# Pic 画像生成
-# https://godoc.org/golang.org/x/tour/pic#example-Show
+// Pic 画像生成
+// https://godoc.org/golang.org/x/tour/pic#example-Show
 func Pic(dx, dy int) [][]uint8 {
-	ss := make([][]uint8,dy)
-	for i:=0;i<dy;i++ {
-		s := make([]uint8,dx)
-		for j:=0;j<dx;j++ {
-			s[j] = (uint8(j)+uint8(i))/2
+	ss := make([][]uint8, dy)
+	for i := 0; i < dy; i++ {
+		s := make([]uint8, dx)
+		for j := 0; j < dx; j++ {
+			s[j] = (uint8(j) + uint8(i)) / 2
 		}
 		ss[i] = s
 	}
