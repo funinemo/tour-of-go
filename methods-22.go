8937a6370e6267f86f5d77077d78871d09006a3a
funinemo@gmail.com <funinemo@gmail.com>
2020-05-17 13:38:51 +0900

    Exercise: Readers

--- methods-22.go
+++ methods-22.go
@@ -8,7 +8,7 @@ type MyReader struct{}
 // TODO: Add a Read([]byte) (int, error) method to MyReader.
 // 参照：https://qiita.com/rock619/items/f412d1f870a022c142d0
 func (r MyReader) Read(b []byte) (n int, err error) {
-	for i, _ := range b {
+	for i := range b {
 		b[i] = 'A'
 	}
 	return len(b), nil
